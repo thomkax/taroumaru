20d3d5ee377547d00f095ec1a50d163142faef2d
Thomka <thomka.x@gmail.com>
2024-04-08 08:55:37 +0300

    verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const font = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="ofIwoRAwnQ5gsEvR3V98Rh1tOOyePPAHJ3mKN6mnCKk"
+        />
+      </Head>
       <body className={font.className}>
         {children}
         <Analytics />

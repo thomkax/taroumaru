399bc14a82122ecc97dfe37acc500f722287479d
Thomka <thomka.x@gmail.com>
2024-04-08 09:00:28 +0300

    verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Genshin Impact Achievements",
   description: "Genshin Impact Achievement Tracker",
+  verification: {
+    google: "ofIwoRAwnQ5gsEvR3V98Rh1tOOyePPAHJ3mKN6mnCKk",
+  },
 };
 
 export default function RootLayout({
@@ -18,12 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="ofIwoRAwnQ5gsEvR3V98Rh1tOOyePPAHJ3mKN6mnCKk"
-        />
-      </Head>
       <body className={font.className}>
         {children}
         <Analytics />
